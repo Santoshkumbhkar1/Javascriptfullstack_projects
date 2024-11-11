46e99bacde0809b4171fa9fe8612f769692bd85a
santosh kumbhkar <santosh.kumbhkareducation3014@gmail.com>
2024-11-11 20:14:38 +0530

    add this file to javascript_full_stack_project

---
+++ BASICS/app.js
@@ -0,0 +1,126 @@
+// console.warn("This is warning ");
+// console.table({Name:"academy" ,age:20});
+
+// Variables  reserved keywords {name } {assigned value}
+// Declared variable 
+ //   var  ,  let  , cosnt by three ways 
+//  let name;
+
+// //  assign value 
+// let banana ;
+// banana =7;
+
+// console.log(name ,banana);
+
+// let  firstVariable = "mrdedication 🤣🤣🤣🤣🤣";
+// firstVariable ="youtubewithlegend"
+// console.log(firstVariable);
+// let is the keywords which is used for declaring variable which can further be change
+
+
+// const variable
+// const name ="me hacker";
+
+// const is the variable which cannot change further called const variable
+//  let num = 1000000;
+// console.log(num);
+
+// // check tyopeof variable by typeog() method
+// console.log(typeof(num));
+
+// Basic maths operation 
+
+// let numb =2;
+// console.log(numb+numb);
+// console.log(numb*numb);
+// console.log(numb**numb);
+// console.log(numb/numb);
+
+
+// Boolean -> true or false 
+
+// let isTrue = false;
+// console.log(isTrue);
+
+// let isProgramminglanguage =true;
+// let isJsHard =false;
+// let favNumb = 25;
+// console.log(favNumb+undefined);
+
+// console.log(10==="10");
+// console.log("hello");
+// console.log(10 ==!10);
+
+let firstFavNumb =25;
+let secondFavNumb=16;
+
+if(firstFavNumb>secondFavNumb){
+    console.log(firstFavNumb);
+    
+}
+else{
+    console.log(secondFavNumb);
+    
+}
+
+// 1.concatation method 
+let fName="rohit";
+let lName =" kumbhkar";
+console.log(fName.concat(lName));
+
+// 2. Append  method
+
+// console.log(fName.Append(lName))
+
+//   3.Length 
+//  console.log(fName.length);
+ 
+// //   4.Cases 
+//  console.log(fName.toLowerCase());
+//  console.log(lName.toUpperCase());
+ 
+// //   5.Slice
+//   let SliceName = fName.slice(0,2);
+//   console.log(SliceName);
+
+
+// //  6. split and Join method
+// console.log((fName.split("")).join("-" ));
+
+// 7. Includes method
+console.log(fName.includes("r"));
+
+// Typeconversion 
+
+let money ="50";
+
+console.log((typeof(money)));
+money =parseInt(money);
+console.log((money));
+console.log((typeof(money)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
