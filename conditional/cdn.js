69f75bc65e8ce4aae6e631aefe5aa6e83beb94f2
santosh kumbhkar <santosh.kumbhkareducation3014@gmail.com>
2024-11-11 21:45:00 +0530

    conditional sentences

---
+++ conditional/cdn.js
@@ -0,0 +1,32 @@
+let  a =20;
+let b  = 20;
+
+if(a>b){
+    console.log("a is grater than b");  
+}
+
+else if(a<b) {
+    console.log("a is less than b ");
+}
+else{
+    console.log("both are equal");
+    
+}
+
+// 2nd 
+
+let time  =20 ;
+let greetings;
+
+if (time<5){
+    greetings ="Good morning😂😂😁😁";
+}
+
+else if (time<20){
+    greetings ="Good morning😂😂😁😁";
+}
+
+else{
+    console.log("balle balle😎😎😍 ");
+    
+}
