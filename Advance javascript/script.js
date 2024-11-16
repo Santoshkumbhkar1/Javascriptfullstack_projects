883cd6532708d66a16c42c2fae15ac81de623fd4
santosh kumbhkar <santosh.kumbhkareducation3014@gmail.com>
2024-11-16 12:23:08 +0530

    some exercise on functions methods

--- Advance javascript/script.js
+++ Advance javascript/script.js
@@ -1,3 +1,4 @@
+'use strict';
 // for printing elements of an array using for of loop
 // const arra1= ["hello " ,"rohan ", "sohan " ,"monu ", "gona"];
 
@@ -74,7 +75,7 @@ const results =peoples.map(person=>{
 })
 
 console.log(results);
-document.write(results);
+
 console.log(peoples);// just for developer purpose.
 
 // filter method filter()
@@ -92,7 +93,69 @@ const songs =[
 // })
 const finSong =songs.filter((n)=>n.duration>3);
 
-document.write(finSong);// it will give only obejct object and we will not used din broser rendering.
+// document.write(finSong);// it will give only obejct object and we will not used din broser rendering.
+
+//   fliter()
+
+const ages =  [32 , 16,  35 ,36];
+function ceckAdults(age){
+  return age>18;
+}
+
+console.log(ages.filter(ceckAdults));
+
+//  create an words array and iterate it 
+//  print only that array string element that has more than 5 length
+
+const word =["Hello " , "namaste" , "pani"  ,"puri" , "dalbati" ,"ghee"];
+
+const wordRes =word.filter((w)=>w.length>5 );
+console.log(wordRes);
+
+// Reduce()  method 
+//  it will make a copy of an array 
+
+// Porblem count the occurence of words in given array 
+
+
+const secWord =[
+      "apple" ,
+      "banana",
+      "banana",
+      "apple" , 
+      "santara" ,
+      "santara",
+
+];
+
+const wordFrequency =secWord.reduce((fmap ,word)=>{
+
+  fmap[word] =(fmap[word]||0)+1
+  // console.log(fmap) ;
+  return fmap;
+} ,{})
+
+console.log(wordFrequency);
+
+//  write a fucntion calculateProduct that takes an array of numbers  as an argument and return the product of array elements.
+
+
+//write a calculateProduct function using reduce method
+
+// declare array 
+
+const numbArray =[1,2,3,4,5];
+// make an function 
+
+function calculateProduct(arr){
+  return arr.reduce((n ,i)=>{
+   return n*i;
+   },1);
+
+};
+const res =calculateProduct(numbArray)
+console.log(res);
+
 
 
 
